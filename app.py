46c3a3be7a0c575e9c00cce7f1b345742ef31fcd
Your Name <dev096m@gmail.com>
2026-05-01 10:37:29 +0530

    portfolio

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/lets-talk")
+def lets_talk():
+    return render_template("chat.html")
+
 if __name__ == "__main__":
     app.run()
